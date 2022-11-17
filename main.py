8bd1cadf9744e54c71ba8653e9deae2d0a99f099
User <romaneefimoff@yandex.ru>
2022-11-17 19:11:42 +0400

    init

--- main.py
+++ main.py
@@ -9,9 +9,19 @@ class CoffeeMachine(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.connection = sqlite3.connect("films_db.sqlite")
+        self.connection = sqlite3.connect("coffee.sqlite")
         self.pushButton.clicked.connect(self.select_data)
-        self.textEdit.setPlainText("SELECT * FROM films")
+        self.textEdit.setPlainText("""SELECT 
+                Bays.time AS time,
+                Flows.direction AS flowDirection,
+                Flows.depth AS flowDepth,
+                Flows.force AS flowForce,
+                Winds.direction AS windDirection,
+                Winds.speed AS windSpeed
+            FROM Bays
+                LEFT JOIN Flows ON Flows.id = Bays.flow_id
+                LEFT JOIN Winds ON Winds.id = Bays.wind_id
+            ORDER BY time""")
         self.select_data()
 
     def select_data(self):
